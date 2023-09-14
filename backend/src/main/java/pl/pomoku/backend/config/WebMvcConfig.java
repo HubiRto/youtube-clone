a8f48c0236f275521f6c2df97de3b1cc14bdbf60
hubirto <hubert.rybicki.hr@gmail.com>
2023-09-14 02:29:46 +0200

    Dodanie konfiguracji CORS

---
+++ backend/src/main/java/pl/pomoku/backend/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package pl.pomoku.backend.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NotNull CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
